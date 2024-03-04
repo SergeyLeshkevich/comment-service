642180d847307340e19f655ca5165548b886ed4f
Sergey Leshkevich <s.leshkevichemail@gmail.com>
2024-03-04 11:39:19 +0300

    add CommentService class

---
+++ src/main/java/ru/clevertec/comment/service/CommentService.java
@@ -0,0 +1,33 @@
+package ru.clevertec.comment.service;
+
+import ru.clevertec.comment.entity.dto.CommentRequest;
+import ru.clevertec.comment.entity.dto.CommentResponse;
+import ru.clevertec.comment.util.PaginationResponse;
+
+import java.util.List;
+
+
+public interface CommentService {
+
+    CommentResponse get(Long id);
+
+    CommentResponse getCommentByNewsId(Long commentId, Long newsId);
+
+    CommentResponse getFromArchive(Long id);
+
+    PaginationResponse<CommentResponse> getAll(int pageSize, int numberPage);
+
+    PaginationResponse<CommentResponse> getAllFromArchive(int pageSize, int numberPage);
+
+    CommentResponse create(CommentRequest commentDto);
+
+    CommentResponse update(Long id, CommentRequest commentDto);
+
+    PaginationResponse<CommentResponse> getCommentsByIdNews(Long idNews, int pageSize, int numberPage);
+    PaginationResponse<CommentResponse> getCommentsByIdNewsFromArchive(Long idNews, int pageSize, int numberPage);
+
+    void archive(Long id);
+
+    void archiveByNewsId(Long newsId);
+    List<CommentResponse> search(String searchValue,  Integer offset, Integer limit);
+}
