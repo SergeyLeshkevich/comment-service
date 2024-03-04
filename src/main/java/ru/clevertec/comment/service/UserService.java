bc4566b9e1883b1c2c6352656122d8eed3fa5a05
Sergey Leshkevich <s.leshkevichemail@gmail.com>
2024-03-04 11:39:37 +0300

    add UserService class

---
+++ src/main/java/ru/clevertec/comment/service/UserService.java
@@ -0,0 +1,12 @@
+package ru.clevertec.comment.service;
+
+import ru.clevertec.comment.entity.User;
+import ru.clevertec.comment.entity.dto.UserRequest;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserService {
+    User create(UserRequest userRequest);
+    Optional<User> getByUuiD(UUID uuid);
+}
